989e8dc9c01bd95b1795a36a67ff01fe331993ee
TheNameKevinWasTaken <kevin.gay@live.ca>
2018-11-14 18:55:19 -0500

    Update perks.py

--- perks.py
+++ perks.py
@@ -1,5 +1,5 @@
-import ctypes
 from random import shuffle
+import tkinter as tk
 
 perk = ["Ace In The Hole",
 "Adrenaline",
@@ -59,7 +59,21 @@ perk = ["Ace In The Hole",
 "Windows Of Opportunity",
 "You Choose"]
 
-shuffle(perk)
-output = "1) " + perk[0] + "\n" + "2) " + perk[1] + "\n" + "3) " + perk[2] + "\n" + "4) " + perk[3] + "\n" + "5) " + perk[4] + "\n" 
+def nextfive():
+    shuffle(perk)
+    output = "1) " + perk[0] + "\n" + "2) " + perk[1] + "\n" + "3) " + perk[2] + "\n" + "4) " + perk[3] + "\n" + "5) " + perk[4]
+    label.config(text=output)
 
-ctypes.windll.user32.MessageBoxW(0, output, "Perks", 1)
+root = tk.Tk()
+root.title("Perks")
+root.geometry("200x85")
+label = tk.Label(root, fg="dark green", anchor="w", justify=tk.LEFT)
+label.pack(side=tk.LEFT)
+nextfive()
+button = tk.Button(root, text='Again', height=5, command=lambda: nextfive())
+button.pack(side=tk.RIGHT)
+root.mainloop()
+    
+
+
+
